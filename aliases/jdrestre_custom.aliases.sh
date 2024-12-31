584f1ae45ab7d203ea4d95a85aef3029631e964f
jdrestre <jdrestre79@gmail.com>
2024-12-30 21:42:14 -0500

    Add alias for Update APT package lists and Upgrade APT packages

--- aliases/jdrestre_custom.aliases.sh
+++ aliases/jdrestre_custom.aliases.sh
@@ -74,5 +74,7 @@ rm_custom() {
 }
 
 
-alias rm='rm_custom'        # Create alias to use the custom rm function
-alias emacs='emacs -nw'     # Run Emacs in terminal mode
+alias rm='rm_custom'          # Create alias to use the custom rm function
+alias emacs='emacs -nw'       # Run Emacs in terminal mode
+alias supd='sudo apt update'  # Update APT package lists
+alias supg='sudo apt upgrade' # Upgrade APT packages
